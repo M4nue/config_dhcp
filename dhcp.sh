beee3b8b69ea464a93746fdd3afc89150f99e632
M4nue <mrodcur12042005@gmail.com>
2025-03-31 11:14:20 +0200

    Plantilla

---
+++ dhcp.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#Autores: Manuel Rodríguez Curado, Jose Manuel Sierra Aroca, Estabaliz Perez Muñoz, Jose María Oteo Dorado
+#Fecha creación: 2025-03-31
+#Descripcion del script:Instalación y configura un servidor dhcp
+#ZONA DECLARACIÓN DE VARIABLES
+
+#ZONA DECLARACIÓN DE FUNCIONES
+
