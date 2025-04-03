b06363d78a000b9dec53c8d326bc46dd1cf91383
M4nue <mrodcur12042005@gmail.com>
2025-04-03 11:56:35 +0200

    implementacion de funciones getops

--- dhcp.sh
+++ dhcp.sh
@@ -1,37 +1,37 @@
 #!/usr/bin/env bash
+#set -x
 #Autores: Manuel Rodríguez Curado, Jose Manuel Sierra Aroca, Estabaliz Perez Muñoz, Jose María Oteo Dorado
 #Fecha creación: 2025-03-31
 #Descripcion del script:Instalación y configura un servidor dhcp
+
 #ZONA DECLARACIÓN DE VARIABLES
-v_paquete_dhcp="isc-dhcp-server"
+paquete="isc-dhcp-server"
+#Expresión regular para identificar si una cadena de caracteres es una IP
+regexp_ip="^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$"
+#Expresión regular confirmar tiempo
+regexp_tiempo="^[1-9][0-9]*$"
+
+
 #ZONA DECLARACIÓN DE FUNCIONES
-
-#Insertar funcion f_soy_root
-
-function f_soyroot1(){
-
-        if [[ $UID -eq 0 ]] ;then
-                return 0
-        else
-                return 1
-        fi
+function f_soy_root(){
+  if [[ $UID -eq 0 ]] ;then
+    return 0
+  else
+    return 1
+  fi
 }
 
 #Insertar funcion f_paquete_instalado
 f_instalado2(){
-  read -p "Introduce el nombre del paquete" paquete
-
-  if f_buscar_paquete "paquete"; then
+  if f_buscar_paquete $paquete; then
     echo "El paquete ya está instalado."
     return 0
   else
     read -p "¿Quiere instalar $paquete? (yes/no): " opcion
     if [[ "$opcion" == "yes" ]]; then
-      if f_hay_conexion; then
-        echo "Conexión a internet detectada. Instalando $paquete..."
-        sudo apt install -y "$paquete"
+      if f_soy_root && f_hay_conexion; then
+        apt install -y "$paquete"
       else
-        echo "No tienes acceso a internet. No se puede instalar el paquete."
         return 1
       fi
     else
@@ -46,12 +46,84 @@ f_instalado2(){
 #Insertar funcion f_conexion_internet
 
 function f_conexion_internet(){
+  local conexion="8.8.8.8"
 
-        local conexion="8.8.8.8"
-
-        if [[ $(ping -c 2 $conexion) ]] ;then
-                return 0
-        else
-                return 1
-        fi
+  if [[ $(ping -c 1 $conexion) ]] ;then
+    return 0
+  else
+    return 1
+  fi
 }
+
+
+f_validar_ip(){
+  if [[ $1 =~ $regexp_ip ]]; then
+    echo "ip valida"
+    return 0
+  else
+    echo "ip invalida"
+    return 1
+  fi
+}
+
+f_validar_red(){
+  if [[ $1 =~ $regexp_ip ]]; then
+    echo "ip de red valida"
+    return 0
+  else
+    echo "ip de red invalida"
+    return 1
+  fi
+}
+
+f_mostrar_configuracion(){
+  if [[ $(cat /etc/dhcpd.conf | grep -Ev "^#|^$") ]]; then
+    return 0
+  else
+    echo "Fallo al encontrar el archivo"
+    return 1
+  fi
+}
+
+f_validar_tiempo(){
+  if [[ $1 =~ ($regexp_tiempo) ]]; then
+    echo "tiempo valida"
+    return 0
+  else
+    echo "tiempo invalido"
+    return 1
+  fi
+}
+
+while getopts "hf:l:sn:t:T:" opcion; do
+  case $opcion in
+    h)echo "Mostrar función de ayuda"
+;;
+    d)echo "Indicar los DNS en la configuracion"
+      f_validar_ip $OPTARG
+;;
+    f)echo "la opción f de first (primera) $opcion cuenta con la primera ip la $OPTARG"
+      f_validar_ip $OPTARG
+;;
+    l)echo "la opcion l de lasted (ultima) $opcion cuenta con la ultima ip $OPTARG"
+      f_validar_ip $OPTARG
+;;
+    s)echo "Opción s de show muestra configuración actual" 
+      f_mostrar_configuracion
+;;
+    n)echo "Opción n de network, se espera parsar una mascara de red en hexadecimal(255.255.255.255)"
+      f_validar_red $OPTARG
+;;
+    t)echo "Tiempo por defecto de la concesion de ip en segundos"
+      f_validar_tiempo $OPTARG
+;;
+    T)echo "Tiempo maximo permitido de concesion de una ip"
+       f_validar_tiempo $OPTARG
+;;
+    ?)echo "Error en la sintaxis del comando, revisa la ayuda"
+;;
+  esac
+done
+
+
+
